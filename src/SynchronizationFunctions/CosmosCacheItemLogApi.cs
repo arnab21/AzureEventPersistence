1949dfefcd5f7826055a41ba797a21bc581e34ee
arnab <arnab21@yahoo.com>
2019-06-26 19:22:21 +0530

    Add Cosmos Item triggered function to print CosmosCache data

---
+++ src/SynchronizationFunctions/CosmosCacheItemLogApi.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AzureEventPersistence.SynchronizationFunctions
+{
+    public static class CosmosCacheItemLogApi
+    {
+        [FunctionName("CosmosCacheItemLogApi")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "CosmosCache",
+            collectionName: "CacheItems",
+            ConnectionStringSetting = "CosmosCacheItemDbConnString",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true)] IReadOnlyList<Document> input, ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                log.LogInformation("Documents modified " + input.Count);
+                LogCosmosCacheMessages(input, log);
+            }
+        }
+
+
+        private static void LogCosmosCacheMessages(IReadOnlyList<Document> documents, ILogger log)
+        {
+            int count = 0;
+            foreach (var doc in documents)
+            {
+                log.LogInformation("Document {count} Id " + doc.Id);
+                log.LogInformation("Document {count} Etag " + doc.ETag);
+                log.LogInformation("Document {count} content " + doc.ToString());
+
+                count++;
+            }
+
+        }
+
+
+    }
+
+
+}
